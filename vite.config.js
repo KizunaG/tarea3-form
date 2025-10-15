b3c34f21046ddccd93abb46fdf64275ba1275295
KizunaG <hecto@LAPTOP-IUQ31NPB>
2025-10-14 22:56:35 -0600

    Inicial: formulario React + Bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ⚠️ Cambia '/tarea3-form/' si tu repo tiene otro nombre
 export default defineConfig({
   plugins: [react()],
+  base: '/tarea3-form/'
 })
